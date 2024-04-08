d0ada3b5ad36754225265ea3c150ef91a4758a77
cwinters01 <cwinters1@ksu.edu>
2024-04-07 22:17:12 -0500

    Updated CMakeLists.txt

--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -7,15 +7,12 @@ set(CMAKE_DEPENDS_GENERATOR "Unix Makefiles")
 # The top level Makefile was generated from the following files:
 set(CMAKE_MAKEFILE_DEPENDS
   "CMakeCache.txt"
-  "C:/Program Files/CMake/share/cmake-3.28/Modules/BasicConfigVersion-AnyNewerVersion.cmake.in"
   "C:/Program Files/CMake/share/cmake-3.28/Modules/CMakeCInformation.cmake"
   "C:/Program Files/CMake/share/cmake-3.28/Modules/CMakeCXXInformation.cmake"
   "C:/Program Files/CMake/share/cmake-3.28/Modules/CMakeCommonLanguageInclude.cmake"
-  "C:/Program Files/CMake/share/cmake-3.28/Modules/CMakeDependentOption.cmake"
   "C:/Program Files/CMake/share/cmake-3.28/Modules/CMakeGenericSystem.cmake"
   "C:/Program Files/CMake/share/cmake-3.28/Modules/CMakeInitializeConfigs.cmake"
   "C:/Program Files/CMake/share/cmake-3.28/Modules/CMakeLanguageInformation.cmake"
-  "C:/Program Files/CMake/share/cmake-3.28/Modules/CMakePackageConfigHelpers.cmake"
   "C:/Program Files/CMake/share/cmake-3.28/Modules/CMakeRCInformation.cmake"
   "C:/Program Files/CMake/share/cmake-3.28/Modules/CMakeSystemSpecificInformation.cmake"
   "C:/Program Files/CMake/share/cmake-3.28/Modules/CMakeSystemSpecificInitialize.cmake"
@@ -23,14 +20,6 @@ set(CMAKE_MAKEFILE_DEPENDS
   "C:/Program Files/CMake/share/cmake-3.28/Modules/Compiler/GNU-C.cmake"
   "C:/Program Files/CMake/share/cmake-3.28/Modules/Compiler/GNU-CXX.cmake"
   "C:/Program Files/CMake/share/cmake-3.28/Modules/Compiler/GNU.cmake"
-  "C:/Program Files/CMake/share/cmake-3.28/Modules/ExternalProject/shared_internal_commands.cmake"
-  "C:/Program Files/CMake/share/cmake-3.28/Modules/FetchContent.cmake"
-  "C:/Program Files/CMake/share/cmake-3.28/Modules/FetchContent/CMakeLists.cmake.in"
-  "C:/Program Files/CMake/share/cmake-3.28/Modules/FindPackageHandleStandardArgs.cmake"
-  "C:/Program Files/CMake/share/cmake-3.28/Modules/FindPackageMessage.cmake"
-  "C:/Program Files/CMake/share/cmake-3.28/Modules/FindPython.cmake"
-  "C:/Program Files/CMake/share/cmake-3.28/Modules/FindPython/Support.cmake"
-  "C:/Program Files/CMake/share/cmake-3.28/Modules/GNUInstallDirs.cmake"
   "C:/Program Files/CMake/share/cmake-3.28/Modules/Platform/Windows-GNU-C.cmake"
   "C:/Program Files/CMake/share/cmake-3.28/Modules/Platform/Windows-GNU-CXX-ABI.cmake"
   "C:/Program Files/CMake/share/cmake-3.28/Modules/Platform/Windows-GNU-CXX.cmake"
@@ -39,21 +28,11 @@ set(CMAKE_MAKEFILE_DEPENDS
   "C:/Program Files/CMake/share/cmake-3.28/Modules/Platform/Windows-windres.cmake"
   "C:/Program Files/CMake/share/cmake-3.28/Modules/Platform/Windows.cmake"
   "C:/Program Files/CMake/share/cmake-3.28/Modules/Platform/WindowsPaths.cmake"
-  "C:/Program Files/CMake/share/cmake-3.28/Modules/WriteBasicConfigVersionFile.cmake"
   "C:/Users/cobyw/Documents/Proj4_CIS520/OS_Proj_4/CMakeLists.txt"
   "CMakeFiles/3.28.1/CMakeCCompiler.cmake"
   "CMakeFiles/3.28.1/CMakeCXXCompiler.cmake"
   "CMakeFiles/3.28.1/CMakeRCCompiler.cmake"
   "CMakeFiles/3.28.1/CMakeSystem.cmake"
-  "_deps/googletest-src/CMakeLists.txt"
-  "_deps/googletest-src/googlemock/CMakeLists.txt"
-  "_deps/googletest-src/googlemock/cmake/gmock.pc.in"
-  "_deps/googletest-src/googlemock/cmake/gmock_main.pc.in"
-  "_deps/googletest-src/googletest/CMakeLists.txt"
-  "_deps/googletest-src/googletest/cmake/Config.cmake.in"
-  "_deps/googletest-src/googletest/cmake/gtest.pc.in"
-  "_deps/googletest-src/googletest/cmake/gtest_main.pc.in"
-  "_deps/googletest-src/googletest/cmake/internal_utils.cmake"
   )
 
 # The corresponding makefile is:
@@ -64,25 +43,11 @@ set(CMAKE_MAKEFILE_OUTPUTS
 
 # Byproducts of CMake generate step:
 set(CMAKE_MAKEFILE_PRODUCTS
-  "_deps/googletest-subbuild/CMakeLists.txt"
   "CMakeFiles/CMakeDirectoryInformation.cmake"
-  "_deps/googletest-build/CMakeFiles/CMakeDirectoryInformation.cmake"
-  "_deps/googletest-build/googletest/generated/gmock.pc"
-  "_deps/googletest-build/googletest/generated/gmock_main.pc"
-  "_deps/googletest-build/googlemock/CMakeFiles/CMakeDirectoryInformation.cmake"
-  "_deps/googletest-build/googletest/generated/GTestConfigVersion.cmake"
-  "_deps/googletest-build/googletest/generated/GTestConfig.cmake"
-  "_deps/googletest-build/googletest/generated/gtest.pc"
-  "_deps/googletest-build/googletest/generated/gtest_main.pc"
-  "_deps/googletest-build/googletest/CMakeFiles/CMakeDirectoryInformation.cmake"
   )
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
   "CMakeFiles/proj4_pthread.dir/DependInfo.cmake"
   "CMakeFiles/hw4_test.dir/DependInfo.cmake"
-  "_deps/googletest-build/googlemock/CMakeFiles/gmock.dir/DependInfo.cmake"
-  "_deps/googletest-build/googlemock/CMakeFiles/gmock_main.dir/DependInfo.cmake"
-  "_deps/googletest-build/googletest/CMakeFiles/gtest.dir/DependInfo.cmake"
-  "_deps/googletest-build/googletest/CMakeFiles/gtest_main.dir/DependInfo.cmake"
   )
