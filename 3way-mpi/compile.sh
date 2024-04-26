206904db383e1885f44f49d2fd73e11295bf1ce7
cwinters01 <111705274+cwinters01@users.noreply.github.com>
2024-04-26 12:25:01 -0500

    Update compile.sh

--- 3way-mpi/compile.sh
+++ 3way-mpi/compile.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-g++ -mpi -o3 proj4_mpi.c -o proj4_mpi
+# Load the desired OpenMPI module
+module load OpenMPI/4.1.4-GCC-11.3.0
+
+# Compile the MPI program using mpicc
+mpicc -O3 mpi.c -o proj4_mpi
